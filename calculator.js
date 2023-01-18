c8d7ba61b658c1d7c6be6968cebf96e9b9a65fc0
Smalhotra1996 <sonam.malhotra2496@gmail.com>
2023-01-18 14:56:48 -0500

    Generated BMI Calculator - Added the get and post methods for the /bmicalculator route

--- calculator.js
+++ calculator.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (request, response) {
   console.log(request);
-  response.send("hello there this is new");
+  response.sendFile(__dirname + "/index.html");
 });
-app.get("/contact", function (request, response) {
+
+app.post("/", function (request, response) {
+  console.log(request.body);
+  var num1 = Number(request.body.n1);
+  var num2 = Number(request.body.n2);
+  var result = num1 + num2;
+  response.status(200).send("<em>Total is <em>" + result);
+});
+
+app.get("/bmiCalculator", function (request, response) {
   console.log(request);
-  response.send("hello hello");
+  response.sendFile(__dirname + "/bmiCalculator.html");
 });
+app.post("/bmiCalculator", function (request, response) {
+  console.log(request.body);
+  var weight = parseFloat(request.body.weight);
+  var height = parseFloat(request.body.height);
+  var bmiCalculator = (weight / height) * height;
+  response.status(200).send("Your BMI is  " + bmiCalculator);
+});
+
 app.listen(3000, function () {
   console.log("server has started on port 3000");
 });
